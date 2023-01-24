5005848a6f59deaece6079f12461f599333d6179
Carlos El Badawi <carlosbadawii@gmail.com>
2023-01-24 20:40:32 +0200

    Corrected java file

---
+++ Problems.java
@@ -0,0 +1,7 @@
+Public class Problem{
+         public static void Main (String[] args){
+		     System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                 System.out.println("Hello, there are a lot"),
+                 System.out.print("of problems");
+         }
+} 

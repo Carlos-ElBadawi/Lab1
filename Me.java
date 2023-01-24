11c61c9ab1a28af2d04fbd5ebcc923b3e1e73647
Carlos El Badawi <carlosbadawii@gmail.com>
2023-01-24 20:47:22 +0200

    My name,major and age

---
+++ Me.java
@@ -0,0 +1,7 @@
+public class Me{
+	public static void main (String[] args){
+              System.out.println("Carlos El Badawi");
+              System.out.println("18 years old");
+              System.out.println("Computer engineering");
+      }
+}
